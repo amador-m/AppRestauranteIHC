50391105a03b9159ed21bbf53cc99d30de2d25c1
Maria Eduarda <dudaamadorm@gmail.com>
2025-11-09 20:56:05 -0300

    Update SplashActivity.kt

--- SistemaRestaurante/app/src/main/java/com/example/sistemarestaurante/SplashActivity.kt
+++ SistemaRestaurante/app/src/main/java/com/example/sistemarestaurante/SplashActivity.kt
@@ -1,7 +1,6 @@
 package com.example.sistemarestaurante
 
 import android.content.Intent
-// IMPORTAÇÃO CORRETA PARA A NOVA ANIMAÇÃO
 import android.view.animation.AnimationUtils
 import android.os.Bundle
 import android.os.Handler
@@ -13,7 +12,6 @@ class SplashActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySplashBinding
 
-    // O delay deve ser o mesmo (ou um pouco maior) que a duração da animação
     private val SPLASH_DELAY: Long = 2100 // 2.1 segundos
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,17 +19,14 @@ class SplashActivity : AppCompatActivity() {
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // 1. Carrega a animação de "zoom/fade" do res/anim
         val splashAnimation = AnimationUtils.loadAnimation(this, R.anim.splash_animation)
-
-        // 2. Aplica a animação no CardView da logo (para o card e a sombra crescerem)
         binding.cvLogo.startAnimation(splashAnimation)
 
-        // 3. O Handler agora só espera a animação terminar
         Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
         }, SPLASH_DELAY)
     }
+
 }
