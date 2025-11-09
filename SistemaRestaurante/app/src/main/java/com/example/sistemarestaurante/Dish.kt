7bd011fa2bb94e37c2b1c768dd9e5b7d5c2744fd
Maria Eduarda <dudaamadorm@gmail.com>
2025-11-09 20:44:33 -0300

    Update Dish.kt

--- SistemaRestaurante/app/src/main/java/com/example/sistemarestaurante/Dish.kt
+++ SistemaRestaurante/app/src/main/java/com/example/sistemarestaurante/Dish.kt
@@ -6,14 +6,12 @@ import kotlinx.parcelize.Parcelize
 @Parcelize
 data class Dish(
     var id: String = "",
-    var name: String = "", // MUDADO DE VAL
-    var description: String = "", // MUDADO DE VAL
-    var price: Double = 0.0, // MUDADO DE VAL
-    var category: String = "", // MUDADO DE VAL
+    var name: String = "", 
+    var description: String = "", 
+    var price: Double = 0.0, 
+    var category: String = "", 
     var isAvailable: Boolean = true,
-    var imageUrl: String = "" // MUDADO DE VAL
+    var imageUrl: String = ""
 ) : Parcelable {
-
-    // Construtor vazio
     constructor() : this("", "", "", 0.0, "", true, "")
 }
